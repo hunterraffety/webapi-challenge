61572b4fc1708976354f74fa36edf909b9376178
hsrsbd <hsraffety@gmail.com>
2019-07-12 09:02:11 -0700

    confused. am i supposed to get all the actions associated with a single project? this endpoint only returns the single action correlating to the id in the parameter.

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -4,4 +4,26 @@ const Actions = require('./actionModel');
 
 const router = express.Router();
 
+router.use(express.json());
+
+// get actions
+router.get('/', (req, res) => {
+  try {
+    res.status(200).send(`<h2>Please provide a project ID</h2>`);
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong.' });
+  }
+});
+
+// get action per project id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const actions = await Actions.get(id);
+    res.status(200).send(actions);
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong.' });
+  }
+});
+
 module.exports = router;
